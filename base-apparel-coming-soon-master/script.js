ed0bbfb635df982f8f6fdf36c7c1e421b36df3aa
KaanArkan <98814210+KaanArkan@users.noreply.github.com>
2026-01-29 21:56:51 +0300

    2 more added

---
+++ base-apparel-coming-soon-master/script.js
@@ -0,0 +1,27 @@
+let email = document.getElementById("email");
+let submitBtn = document.getElementById("submit-btn");
+
+submitBtn.addEventListener("click", function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    validateEmail(email);
+});
+email.addEventListener("blur", function() {
+    validateEmail(email);
+});
+
+function validateEmail(input) {
+    let inputArea = input.parentElement.parentElement;
+    let errorMessage = inputArea.querySelector(".error-text");
+    let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (input.value.trim() === "") {
+        inputArea.classList.add("hasError");
+        errorMessage.textContent = `Please provide an email address`;
+    } else if (!emailPattern.test(input.value.trim())) {
+        inputArea.classList.add("hasError");
+        errorMessage.textContent = "Please provide a valid email address";
+    } else {
+        inputArea.classList.remove("hasError");
+        errorMessage.textContent = "";
+    }
+}
